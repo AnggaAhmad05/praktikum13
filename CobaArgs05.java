2767b2f733b73beae52c8f776ed8d95492b82541
AnggaAhmad05 <anggaahmad9027@gmail.com>
2023-11-29 14:49:07 +0700

    Args

--- CobaArgs05.java
+++ CobaArgs05.java
@@ -1,6 +1,12 @@
 public class CobaArgs05 {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        
+        if (1 < args.length) {
+            System.out.println("Hai " + args[0]);
+            System.out.println("Selamat datang di " + args[1]);
+        } else {
+            System.out.println("Hai, selamat datang");
+        }
     
 }
     
