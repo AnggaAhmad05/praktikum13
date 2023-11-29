6f3080ea5476834b4d21eb1ee72c996776c4c666
AnggaAhmad05 <anggaahmad9027@gmail.com>
2023-11-29 10:58:39 +0700

    fungsi

---
+++ Welcome05.java
@@ -0,0 +1,9 @@
+public class Welcome05 {
+    public static void displayWelcomeMessage(){
+        System.out.println("Selamat datang di Politeknik Negeri Malang");
+
+    }
+    public static void main(String[] args) {
+        displayWelcomeMessage();
+    }
+}
